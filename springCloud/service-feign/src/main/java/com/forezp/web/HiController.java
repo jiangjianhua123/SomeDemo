081df81a3ef12ece1e5bf89ed64e37081c1b7bc5
jiangjianhua123 <jiangjianhua123@qq.com>
2018-08-30 22:38:55 +0800

    update

---
+++ springCloud/service-feign/src/main/java/com/forezp/web/HiController.java
@@ -0,0 +1,21 @@
+package com.forezp.web;
+
+import com.forezp.service.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+
+    @GetMapping(value = "/hi")
+    public String sayHi(@RequestParam String name){
+        return schedualServiceHi.sayHiFromeClientOne(name);
+    }
+
+}
